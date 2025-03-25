fb47cc2b4468597448f42059fac267113f549d1d
user.email <leovoon>
2025-03-25 22:59:09 +0800

    undo edit

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			// See below for an explanation of these options
 			routes: {
 				include: ['/*'],
-				exclude: []
+				exclude: ['<all>']
 			},
 			platformProxy: {
 				configPath: undefined,
